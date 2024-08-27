8f6811717ce0ad228b70e12392f44560fca774a4
alfonsogalguera <57268703+alfonsogalguera@users.noreply.github.com>
2024-08-27 13:46:35 -0600

    portafolio
    portafolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="text-gray-800  py-8 shadow-lg">
+      <div className="">
+        {/* Botón del menú, solo visible en pantallas pequeñas */}
+        <button 
+          onClick={toggleMenu} 
+          className="text-black block lg:hidden focus:outline-none ml-8"
+        >
+          <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg>
+        </button>
+
+        {/* Navegación, visible en pantallas grandes y cuando se abre el menú en pantallas pequeñas */}
+        <nav className={`flex lg:space-x-8 justify-center items-center flex-col lg:flex-row ${isOpen ? 'block' : 'hidden'}`}>
+          <a href="#educacion" className="hover:text-gray-400 block py-2 lg:py-0">Home</a>
+          <a href="#experiencia" className="hover:text-gray-400 block py-2 lg:py-0">About</a>
+          <a href="#proyectos" className="hover:text-gray-400 block py-2 lg:py-0">Projects</a>
+          <a href="#sobre-mi" className="hover:text-gray-400 block py-2 lg:py-0">Contact</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen bg-white text-black lg:px-28">
+      {/* Lado Izquierdo */}
+      <div className="lg:w-1/2 p-8 text-center ">
+        <h1 className="text-4xl lg:text-6xl font-bold mb-4 text-center">
+          Alfonso Galguera Caballero
+        </h1>
+        <h2 className="text-2xl lg:text-4xl mb-6 text-center">
+          Desarrollador Frontend
+        </h2>
+        <p className="text-lg lg:text-xl text-gray-400 text-justify">
+          Apasionado por crear interfaces web modernas y eficientes, con un
+          enfoque en la experiencia del usuario y el rendimiento. Siempre en
+          busca de aprender y aplicar nuevas tecnologías en el desarrollo
+          frontend.
+        </p>
+        <button
+          onClick={() => window.open("/ruta/a/tu-cv.pdf", "_blank")}
+          className="font-bold py-2 px-4 rounded-lg mt-12 border-2 border-green-300 hover:border-green-800 transition duration-300"
+        >
+          Descargar CV
+        </button>
+      </div>
+
+      {/* Lado Derecho */}
+      <div className="lg:w-1/2 p-8">
+        <img
+          src="/src/assets/images/main.avif"
+          alt="Programación"
+          className="w-auto h-auto flex justify-end"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
